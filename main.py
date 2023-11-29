8b00cfe8894f6ee9f07f1be33806bd6ee952f313
jinseisieko <jinseisieko@gmail.com>
2023-11-29 21:59:49 +0300

    поменял форму на эллипс, который меняет цвета при касании

--- main.py
+++ main.py
@@ -19,20 +19,25 @@ class Rectangle:
         self.minus_y = 0
         self.rect = pygame.Surface((width, height))
         self.rect.fill((255, 255, 255, 128))
+        self.color = [0, 0, 255]
 
     def update(self):
         self.x += self.velocity * math.cos(self.angle) * (-1) ** self.minus_x
         self.y += self.velocity * math.sin(self.angle) * (-1) ** self.minus_y
         if self.x + self.width > FIELD_WIDTH:
+            self.color = [random.randint(0, 255) for _ in range(3)]
             self.minus_x = 1
             self.x = FIELD_WIDTH - self.width
         if self.y + self.height > FIELD_HEIGHT:
+            self.color = [random.randint(0, 255) for _ in range(3)]
             self.minus_y = 1
             self.y = FIELD_HEIGHT - self.height
         if self.y < 0:
+            self.color = [random.randint(0, 255) for _ in range(3)]
             self.minus_y = 0
             self.y = 0
         if self.x < 0:
+            self.color = [random.randint(0, 255) for _ in range(3)]
             self.minus_x = 0
             self.x = 0
 
@@ -43,6 +48,7 @@ screen: pygame.Surface = pygame.display.set_mode((FIELD_WIDTH, FIELD_HEIGHT))
 rectangle = Rectangle(200, 100, 0, 0, 0.5)
 clock = pygame.time.Clock()
 
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -51,8 +57,10 @@ while running:
             break
 
     rectangle.update()
-    screen.fill(([random.randint(0, 255) for _ in range(3)]))
-    screen.blit(rectangle.rect, (rectangle.x, rectangle.y))
+    screen.fill((0, 0, 0))
+    pygame.draw.ellipse(screen, rectangle.color,
+                        (rectangle.x, rectangle.y, rectangle.rect.get_size()[0], rectangle.rect.get_size()[1]))
+    # screen.blit(rectangle.rect, (rectangle.x, rectangle.y))
 
     pygame.display.flip()
     clock.tick(120)
